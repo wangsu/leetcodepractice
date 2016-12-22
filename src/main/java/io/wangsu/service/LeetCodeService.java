4a42795bf517e7cb85b07f6853835d16f8f89c6d
Su Wang <SuW@minicircuits.com>
2016-12-22 11:48:28 -0500

    283. Move Zeroes Better Solution

--- src/main/java/io/wangsu/service/LeetCodeService.java
+++ src/main/java/io/wangsu/service/LeetCodeService.java
@@ -15,10 +15,26 @@ public class LeetCodeService {
     private static final Logger log = LoggerFactory.getLogger(LeetCodeService.class);
 
     /**
-     * 283. Move Zeroes
+     * 283. Move Zeroes Better Solution
      * @param nums
      */
     public void moveZeroes(int[] nums) {
+        int nonZero = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[nonZero++]=nums[i];
+            }
+        }
+        for(int i=nonZero;i<nums.length;i++){
+            nums[i]=0;
+        }
+    }
+
+    /**
+     * 283. Move Zeroes
+     * @param nums
+     */
+    public void moveZeroes2(int[] nums) {
         for(int i=0;i<nums.length;i++){
             if(nums[i]==0){
                 for(int j=i+1;j<nums.length;j++){
