57a0d4188a34f83f6c320f6ebcf18ec4f01c8119
Su Wang <wangsu@wangsu.io>
2017-05-08 22:54:37 -0400

    12. Integer to Roman

--- src/main/java/io/wangsu/service/LeetCodeMediumService.java
+++ src/main/java/io/wangsu/service/LeetCodeMediumService.java
@@ -208,4 +208,24 @@ public class LeetCodeMediumService {
         }
         return sb.toString();
     }
+
+    /**
+     * 12. Integer to Roman
+     * @param num
+     * @return
+     */
+    public String intToRoman(int num) {
+        int[] values = new int[]{1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] strs = new String[]{"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+        StringBuilder result = new StringBuilder();
+        for(int i=0;i<values.length;i++){
+            while(num>=values[i]){
+                num = num-values[i];
+                result.append(strs[i]);
+            }
+        }
+        return result.toString();
+
+    }
 }
