5a63c78b382c1560078d6b545bb975b60a4fc01a
Su Wang <wangsu@wangsu.io>
2017-07-08 22:30:55 -0400

    46. Permutations

--- src/main/java/io/wangsu/service/LeetCodeMediumService.java
+++ src/main/java/io/wangsu/service/LeetCodeMediumService.java
@@ -700,4 +700,27 @@ public class LeetCodeMediumService {
         }
         return sb.length()==0?"0":sb.toString();
     }
+
+    /**
+     * 46. Permutations
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        permuteHelper(nums,new ArrayList<Integer>(),result);
+        return result;
+    }
+
+    private void permuteHelper(int[] nums, List<Integer> list, List<List<Integer>> result){
+        if(list.size()==nums.length){
+            result.add(new ArrayList(list));
+        }
+        for(int i=0;i<nums.length;i++){
+            if(list.contains(nums[i]))continue;
+            list.add(nums[i]);
+            permuteHelper(nums, list, result);
+            list.remove(list.size()-1);
+        }
+    }
 }
