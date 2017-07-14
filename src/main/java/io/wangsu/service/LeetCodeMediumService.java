3f9b429d40383ebd1d7df280967850f241e89934
Su Wang <wangsu@wangsu.io>
2017-07-14 17:16:58 -0400

    63. Unique Paths II

--- src/main/java/io/wangsu/service/LeetCodeMediumService.java
+++ src/main/java/io/wangsu/service/LeetCodeMediumService.java
@@ -1018,4 +1018,44 @@ public class LeetCodeMediumService {
         }
         return paths[m][n];
     }
+
+    /**
+     * 63. Unique Paths II
+     * @param obstacleGrid
+     * @return
+     */
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if(obstacleGrid==null||obstacleGrid.length==0||obstacleGrid[0].length==0||obstacleGrid[0][0]==1){
+            return 0;
+        }
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] result = new int[m][n];
+        for(int i=0;i<m;i++){
+            if(obstacleGrid[i][0]==0){
+                result[i][0]=1;
+            }else{
+                break;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(obstacleGrid[0][i]==0){
+                result[0][i]=1;
+            }else{
+                break;
+            }
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(obstacleGrid[i][j]==1){
+                    result[i][j]=0;
+                }else{
+                    result[i][j]=result[i-1][j]+result[i][j-1];
+                }
+
+            }
+        }
+        return result[m-1][n-1];
+
+    }
 }
