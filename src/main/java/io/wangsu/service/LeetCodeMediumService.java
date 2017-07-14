1bfdd15e02affa94141158de9b84e4a18a84631e
Su Wang <wangsu@wangsu.io>
2017-07-14 05:01:17 -0400

    62. Unique Paths

--- src/main/java/io/wangsu/service/LeetCodeMediumService.java
+++ src/main/java/io/wangsu/service/LeetCodeMediumService.java
@@ -1000,4 +1000,22 @@ public class LeetCodeMediumService {
 
         return head;
     }
+
+
+
+    /**
+     * 62. Unique Paths
+     * @param m
+     * @param n
+     * @return
+     */
+    static int[][] paths = new int[101][101];
+    public int uniquePaths(int m, int n) {
+        if(m<=0||n<=0) return 0;
+        if(m==1||n==0) return 1;
+        if(paths[m][n]==0){
+            paths[m][n] = uniquePaths(m-1,n)+uniquePaths(m,n-1);
+        }
+        return paths[m][n];
+    }
 }
