bc80ca40500d8958ca89da2a0d178e0a2c05ba10
Su Wang <wangsu@wangsu.io>
2017-07-17 19:32:36 -0400

    64. Minimum Path Sum

--- src/main/java/io/wangsu/service/LeetCodeMediumService.java
+++ src/main/java/io/wangsu/service/LeetCodeMediumService.java
@@ -1058,4 +1058,29 @@ public class LeetCodeMediumService {
         return result[m-1][n-1];
 
     }
+
+    /**
+     * 64. Minimum Path Sum
+     * @param grid
+     * @return
+     */
+    public int minPathSum(int[][] grid) {
+        if(grid==null||grid.length==0) return 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] result = new int[m][n];
+        result[0][0] = grid[0][0];
+        for(int i=1;i<m;i++){
+            result[i][0] = grid[i][0]+result[i-1][0];
+        }
+        for(int i=1;i<n;i++){
+            result[0][i] = grid[0][i]+result[0][i-1];
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                result[i][j] = Math.min(result[i-1][j],result[i][j-1])+grid[i][j];
+            }
+        }
+        return result[m-1][n-1];
+    }
 }
