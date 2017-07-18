52e3648ee814b73082dd10d10b7afa96b8ac0393
Su Wang <wangsu@wangsu.io>
2017-07-18 09:28:03 -0400

    74. Search a 2D Matrix

--- src/main/java/io/wangsu/service/LeetCodeMediumService.java
+++ src/main/java/io/wangsu/service/LeetCodeMediumService.java
@@ -1161,4 +1161,30 @@ public class LeetCodeMediumService {
         }
 
     }
+
+    /**
+     * 74. Search a 2D Matrix
+     * @param matrix
+     * @param target
+     * @return
+     */
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix==null||matrix.length==0)return false;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int start = 0;
+        int end = m*n-1;
+        while(start<=end){
+            int mid = (start+end)/2;
+            int row = mid/n;
+            int col = mid%n;
+            if(matrix[row][col]==target)return true;
+            else if(matrix[row][col]>target){
+                end = mid - 1;
+            }else{
+                start = mid +1;
+            }
+        }
+        return false;
+    }
 }
