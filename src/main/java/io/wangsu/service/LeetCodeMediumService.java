a02dcc9cd2fa1be8336ef58f6ad9f4a483487300
Su Wang <wangsu@wangsu.io>
2017-09-15 12:10:36 -0400

    96. Unique Binary Search Trees

--- src/main/java/io/wangsu/service/LeetCodeMediumService.java
+++ src/main/java/io/wangsu/service/LeetCodeMediumService.java
@@ -1573,4 +1573,21 @@ public class LeetCodeMediumService {
         }
         return result;
     }
+
+    /**
+     * 96. Unique Binary Search Trees
+     * @param n
+     * @return
+     */
+    public int numTrees(int n) {
+        int[] result = new int[n+1];
+        result[0]=1;
+        result[1]=1;
+        for(int i=2;i<=n;i++){
+            for(int j=0;j<i;j++){
+                result[i]+=result[j]*result[i-j-1];
+            }
+        }
+        return result[n];
+    }
 }
