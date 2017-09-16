456b7ddf63f8a7953f30e7f3f974f3399858eb66
Su Wang <wangsu@wangsu.io>
2017-09-16 16:21:26 -0400

    105. Construct Binary Tree from Preorder and Inorder Traversal

--- src/main/java/io/wangsu/service/LeetCodeMediumService.java
+++ src/main/java/io/wangsu/service/LeetCodeMediumService.java
@@ -1653,4 +1653,27 @@ public class LeetCodeMediumService {
         zigzagLevelOrderHelper(result,root.left,height+1);
         zigzagLevelOrderHelper(result,root.right,height+1);
     }
+
+    /**
+     * 105. Construct Binary Tree from Preorder and Inorder Traversal
+     * @param preorder
+     * @param inorder
+     * @return
+     */
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return buildTreeHelper(0,0,inorder.length-1,preorder,inorder);
+    }
+
+    private TreeNode buildTreeHelper(int preStart, int inStart, int inEnd, int[] preorder, int[] inorder){
+        if(preStart>preorder.length-1||inStart>inEnd)return null;
+        TreeNode root = new TreeNode(preorder[preStart]);
+        int inIndex = 0;
+        for(int i=inStart;i<=inEnd;i++){
+            if(preorder[preStart]==inorder[i])
+                inIndex = i;
+        }
+        root.left=buildTreeHelper(preStart+1,inStart,inIndex-1,preorder,inorder);
+        root.right=buildTreeHelper(preStart+1+inIndex-inStart,inIndex+1,inEnd,preorder,inorder);
+        return root;
+    }
 }
