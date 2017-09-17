db31c6ee92dcb76687f237a3b2dfa023e7c0c0f9
Su Wang <wangsu@wangsu.io>
2017-09-16 20:16:49 -0400

    109. Convert Sorted List to Binary Search Tree

--- src/main/java/io/wangsu/service/LeetCodeMediumService.java
+++ src/main/java/io/wangsu/service/LeetCodeMediumService.java
@@ -1699,4 +1699,28 @@ public class LeetCodeMediumService {
         root.right = buildTreePostInHepler(inorder, postorder, inIndex+1, inEnd, postStart+inIndex-inStart, postEnd-1, inOrderIndexMap);
         return root;
     }
+
+    /**
+     * 109. Convert Sorted List to Binary Search Tree
+     * @param head
+     * @return
+     */
+    public TreeNode sortedListToBST(ListNode head) {
+        if(head==null)return null;
+        return toBST(head,null);
+    }
+
+    private TreeNode toBST(ListNode head, ListNode tail){
+        ListNode slow = head;
+        ListNode fast = head;
+        if(head==tail)return null;
+        while(fast!=tail&&fast.next!=tail){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        TreeNode root = new TreeNode(slow.val);
+        root.left = toBST(head,slow);
+        root.right = toBST(slow.next,tail);
+        return root;
+    }
 }
