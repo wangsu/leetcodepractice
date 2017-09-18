8a646c5407aef86073784f0e78153659e0776699
Su Wang <wangsu@wangsu.io>
2017-09-17 20:34:59 -0400

    117. Populating Next Right Pointers in Each Node II

--- src/main/java/io/wangsu/service/LeetCodeMediumService.java
+++ src/main/java/io/wangsu/service/LeetCodeMediumService.java
@@ -1792,4 +1792,39 @@ public class LeetCodeMediumService {
         connect(root.left);
         connect(root.right);
     }
+
+    /**
+     * 117. Populating Next Right Pointers in Each Node II
+     * @param root
+     */
+    public void connect2(TreeLinkNode root) {
+        if(root==null)return;
+        TreeLinkNode head = null;
+        TreeLinkNode prev = null;
+        TreeLinkNode cur = root;
+        while(cur!=null){
+            while(cur!=null){
+                if(cur.left!=null){
+                    if(prev!=null){
+                        prev.next = cur.left;
+                    }else{
+                        head = cur.left;
+                    }
+                    prev=cur.left;
+                }
+                if(cur.right!=null){
+                    if(prev!=null){
+                        prev.next=cur.right;
+                    }else{
+                        head=cur.right;
+                    }
+                    prev=cur.right;
+                }
+                cur=cur.next;
+            }
+            cur=head;
+            prev=null;
+            head=null;
+        }
+    }
 }
