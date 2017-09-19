46e99b83b8409a8fb84a2363aeed47d530a53156
Su Wang <wangsu@wangsu.io>
2017-09-18 20:11:58 -0400

    120. Triangle

--- src/main/java/io/wangsu/service/LeetCodeMediumService.java
+++ src/main/java/io/wangsu/service/LeetCodeMediumService.java
@@ -1792,4 +1792,19 @@ public class LeetCodeMediumService {
         connect(root.left);
         connect(root.right);
     }
+
+    /**
+     * 120. Triangle
+     * @param triangle
+     * @return
+     */
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int[] result = new int[triangle.size()+1];
+        for(int i=triangle.size()-1;i>=0;i--){
+            for(int j=0;j<triangle.get(i).size();j++){
+                result[j]=Math.min(result[j],result[j+1])+triangle.get(i).get(j);
+            }
+        }
+        return result[0];
+    }
 }
