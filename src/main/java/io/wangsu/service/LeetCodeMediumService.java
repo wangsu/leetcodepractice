7c1f52f1da74dfef0831e1357fed0c161eb6edce
Su Wang <wangsu@wangsu.io>
2017-09-19 20:18:37 -0400

    137. Single Number II

--- src/main/java/io/wangsu/service/LeetCodeMediumService.java
+++ src/main/java/io/wangsu/service/LeetCodeMediumService.java
@@ -2041,4 +2041,24 @@ public class LeetCodeMediumService {
         }
         return exists[s.length()];
     }
+
+    /**
+     * 137. Single Number II
+     * @param nums
+     * @return
+     */
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for(int i=0;i<32;i++){
+            int sum = 0;
+            for(int num:nums){
+                sum += (((num>>i) & 1) == 1)?1:0;
+                sum %= 3;
+            }
+            if(sum!=0){
+                result |= (sum<<i);
+            }
+        }
+        return result;
+    }
 }
