e5fd8f3bfada73c270ad6c6addc6376ec0fa202f
Su Wang <wangsu@wangsu.io>
2017-09-21 22:29:34 -0400

    25. Reverse Nodes in k-Group

--- src/main/java/io/wangsu/service/LeetCodeMediumService.java
+++ src/main/java/io/wangsu/service/LeetCodeMediumService.java
@@ -2297,4 +2297,40 @@ public class LeetCodeMediumService {
         }
         return max;
     }
+
+    /**
+     * 25. Reverse Nodes in k-Group
+     * @param head
+     * @param k
+     * @return
+     */
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if(head==null||k==1) return head;
+        ListNode dummy=new ListNode(0);
+        dummy.next=head;
+        ListNode pre=dummy;
+        int count=0;
+        while(head!=null){
+            count++;
+            if(count%k==0){
+                pre=reverse(pre,head.next);
+                head=pre.next;
+            }else{
+                head=head.next;
+            }
+        }
+        return dummy.next;
+    }
+
+    private ListNode reverse(ListNode pre,ListNode nextStart){
+        ListNode last=pre.next;
+        ListNode cur=last.next;
+        while(cur!=nextStart){
+            last.next=cur.next;
+            cur.next=pre.next;
+            pre.next=cur;
+            cur=last.next;
+        }
+        return last;
+    }
 }
