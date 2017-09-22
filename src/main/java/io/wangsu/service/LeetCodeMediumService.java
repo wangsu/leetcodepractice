742d084056f52ac0ceb6ed156aa2bc8e82e9894e
Su Wang <wangsu@wangsu.io>
2017-09-22 15:47:29 -0400

    150. Evaluate Reverse Polish Notation

--- src/main/java/io/wangsu/service/LeetCodeMediumService.java
+++ src/main/java/io/wangsu/service/LeetCodeMediumService.java
@@ -2390,4 +2390,35 @@ public class LeetCodeMediumService {
         }
         return dp[amount]>amount?-1:dp[amount];
     }
+
+    /**
+     * 150. Evaluate Reverse Polish Notation
+     * @param tokens
+     * @return
+     */
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack();
+        for(int i=0;i<tokens.length;i++){
+            switch(tokens[i]){
+                case "+":
+                    stack.push(stack.pop()+stack.pop());
+                    break;
+                case "-":
+                    stack.push(-stack.pop()+stack.pop());
+                    break;
+                case "*":
+                    stack.push(stack.pop()*stack.pop());
+                    break;
+                case "/":
+                    int n1 = stack.pop();
+                    int n2 = stack.pop();
+                    stack.push(n2/n1);
+                    break;
+                default:
+                    stack.push(Integer.parseInt(tokens[i]));
+                    break;
+            }
+        }
+        return stack.pop();
+    }
 }
