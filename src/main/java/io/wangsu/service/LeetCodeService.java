ff42f73087a15574bdcb854c15e2a37b1d4b1205
Su Wang <wangsu@wangsu.io>
2017-10-25 15:16:19 -0400

    160. Intersection of Two Linked Lists

--- src/main/java/io/wangsu/service/LeetCodeService.java
+++ src/main/java/io/wangsu/service/LeetCodeService.java
@@ -1339,4 +1339,37 @@ public class LeetCodeService {
         }
         return false;
     }
+
+    /**
+     * 160. Intersection of Two Linked Lists
+     * @param headA
+     * @param headB
+     * @return
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int lenA = getLength(headA);
+        int lenB = getLength(headB);
+        while(lenA>lenB){
+            headA = headA.next;
+            lenA--;
+        }
+        while(lenB>lenA){
+            headB = headB.next;
+            lenB--;
+        }
+        while(headA!=headB){
+            headA = headA.next;
+            headB = headB.next;
+        }
+        return headA;
+    }
+
+    private int getLength(ListNode ln){
+        int result= 0;
+        while(ln!=null){
+            ln=ln.next;
+            result++;
+        }
+        return result;
+    }
 }
