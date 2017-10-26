24fb85f4c39558972482c91b4f9070c0fa43438d
Su Wang <wangsu@wangsu.io>
2017-10-25 22:13:50 -0400

    169. Majority Element

--- src/main/java/io/wangsu/service/LeetCodeService.java
+++ src/main/java/io/wangsu/service/LeetCodeService.java
@@ -1414,4 +1414,25 @@ public class LeetCodeService {
         }
         return result.toString();
     }
+
+    /**
+     * 169. Majority Element
+     * @param nums
+     * @return
+     */
+    public int majorityElement(int[] nums) {
+        int result = nums[0];
+        int count = 1;
+        for(int i=1;i<nums.length;i++){
+            if(count==0){
+                count++;
+                result = nums[i];
+            }else if(result!=nums[i]){
+                count--;
+            }else{
+                count++;
+            }
+        }
+        return result;
+    }
 }
